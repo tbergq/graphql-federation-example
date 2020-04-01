a93d3fadcf4412541b1fe6dfc5e2c78056b25730
Trond Bergquist <trond_bergquist@hotmail.com>
2020-04-01 12:58:36 +0200

    Add posts query

---
+++ src/posts/src/posts/resolvers/Posts.js
@@ -0,0 +1,16 @@
+// @flow
+
+import type { DataSource } from '../../dataSources';
+import type { Post } from '../../dataSources/Posts';
+
+type Context = {
+  +dataSources: DataSource,
+};
+
+export default function Users(
+  _: mixed,
+  { userId }: { +userId: string },
+  { dataSources }: Context,
+): Promise<Post> {
+  return dataSources.posts.getPostsByUserId(userId);
+}
--- src/posts/src/resolvers.js
+++ src/posts/src/resolvers.js
@@ -1,12 +1,14 @@
 // @flow
 
 import post from './posts/resolvers/Post';
+import posts from './posts/resolvers/Posts';
 import Post from './posts/types/Post';
 import User from './posts/types/User';
 
 const resolvers = {
   Query: {
     post,
+    posts,
   },
   Post,
   User,
