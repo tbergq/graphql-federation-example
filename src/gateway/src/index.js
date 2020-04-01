6dde2795b8a85d7d42ed031c8230af8566b14fd4
Trond Bergquist <trond_bergquist@hotmail.com>
2020-04-01 10:25:53 +0200

    Setup the gateway

--- src/gateway/src/index.js
+++ src/gateway/src/index.js
@@ -1,1 +1,26 @@
 // @flow
+
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
+
+// Initialize an ApolloGateway instance and pass it an array of
+// your implementing service names and URLs
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'accounts', url: 'http://localhost:4001' },
+    // Define additional services here
+  ],
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+
+  // Disable subscriptions (not currently supported with ApolloGateway)
+  subscriptions: false,
+});
+
+server.listen(4000).then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server ready at ${url}`);
+});
